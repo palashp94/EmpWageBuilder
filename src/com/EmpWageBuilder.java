3e0bc8bfbd2aedb1e66d9380a629bfa2f922e9b1
palashp94 <palashmadney@gmail.com>
2022-03-11 12:26:41 +0530

    Modified:changed the variable name into capital letter

--- src/com/EmpWageBuilder.java
+++ src/com/EmpWageBuilder.java
@@ -1,14 +1,14 @@
 package com;
 
-public class EmpWageBuilderUC1 {
+public class EmpWageBuilder {
     public static void main(String[] args) {
         // Constants
-        int Is_PART_TIME = 1;
-        int Is_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int NUM_WORKING_DAYS = 20;
         int WAGE_PER_HOUR = 20;
-        int Full_Time_WorkHour = 8;
-        int PART_TIME_WorkHour = 4;
+        int FULL_TIME_WORK_HOUR = 8;
+        int PART_TIME_WORK_HOUR = 4;
         // variables
         int workingDay=0;
         int totalSalary = 0;
@@ -16,12 +16,12 @@ public class EmpWageBuilderUC1 {
             int empWage = 0;
             // Computation
             double empCheck = Math.floor(Math.random() * 10) % 3;
-            if (empCheck == Is_FULL_TIME) {
+            if (empCheck == IS_FULL_TIME) {
                 System.out.println("Employee is Present" + Math.random());
-                empWage = Full_Time_WorkHour * WAGE_PER_HOUR;
-            } else if (empCheck == Is_PART_TIME) {
+                empWage = FULL_TIME_WORK_HOUR * WAGE_PER_HOUR;
+            } else if (empCheck == IS_PART_TIME) {
                 System.out.println("Employee is Present" + Math.random());
-                empWage = PART_TIME_WorkHour * WAGE_PER_HOUR;
+                empWage = PART_TIME_WORK_HOUR * WAGE_PER_HOUR;
             } else
                 System.out.println("Employee is Absent");
             System.out.println("Employee wage: " + empWage);
